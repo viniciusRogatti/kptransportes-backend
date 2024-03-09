987ea519a1eadd0367f7c0ac75acef33ed317345
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-03-09 16:44:42 -0300

    aumentando o limite

--- src/routes/uploads.routes.js
+++ src/routes/uploads.routes.js
@@ -5,7 +5,12 @@ const UploadController = require('../controllers/UploadController');
 const uploadRoutes = Router();
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 10,
+  },
+});
 
 uploadRoutes.post('/', upload.array('files', 5), UploadController.UploadFiles);
 
