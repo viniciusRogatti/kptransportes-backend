a5e10b2a4d0684fe81b9f17a94c64c805a651cba
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-03-09 18:27:51 -0300

    passando o exec para dentro da callback

--- src/services/UploadService.js
+++ src/services/UploadService.js
@@ -12,39 +12,46 @@ const processUpload = async (files) => {
       fs.mkdirSync(uploadDirectory);
     }
 
+    // Contador para verificar se todos os arquivos foram movidos
+    let filesMovedCount = 0;
+
     // Salve os arquivos no diretório "uploads"
     files.forEach((file) => {
       if (file && file.name) {
         const filePath = path.join(uploadDirectory, file.name);
-        try {
-          file.mv(filePath, (err) => {
-            if (err) {
-              console.error(`Erro ao mover o arquivo ${file.name}: ${err}`);
-            } else {
-              console.log(`Arquivo ${file.name} movido com sucesso para ${uploadDirectory}`);
+        file.mv(filePath, (err) => {
+          if (err) {
+            console.error(`Erro ao mover o arquivo ${file.name}: ${err}`);
+          } else {
+            console.log(`Arquivo ${file.name} movido com sucesso para ${uploadDirectory}`);
+            filesMovedCount++;
+
+            // Verifica se todos os arquivos foram movidos antes de executar xmlProcessor.js
+            if (filesMovedCount === files.length) {
+              executeXmlProcessor();
             }
-          });
-        } catch (err) {
-          console.error(`Erro ao processar o arquivo ${file.name}: ${err}`);
+          }
+        });
+      }
+    });
+
+    const executeXmlProcessor = () => {
+      // Imprima uma mensagem ou retorne uma resposta ao frontend, se necessário
+      console.log('Arquivos salvos em', uploadDirectory);
+
+      // Execute o comando "node xmlProcessor.js" aqui
+      exec('node xmlProcessor.js', (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Erro ao executar xmlProcessor.js: ${error.message}`);
+          return;
         }
-      }
-    });
-
-    // Imprima uma mensagem ou retorne uma resposta ao frontend, se necessário
-    console.log('Arquivos salvos em', uploadDirectory);
-
-    // Execute o comando "node xmlProcessor.js" aqui
-    exec('node xmlProcessor.js', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Erro ao executar xmlProcessor.js: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Erro ao executar xmlProcessor.js: ${stderr}`);
-        return;
-      }
-      console.log(`xmlProcessor.js executado com sucesso: ${stdout}`);
-    });
+        if (stderr) {
+          console.error(`Erro ao executar xmlProcessor.js: ${stderr}`);
+          return;
+        }
+        console.log(`xmlProcessor.js executado com sucesso: ${stdout}`);
+      });
+    };
 
     return { success: true, message: 'Arquivos processados com sucesso!' };
   } catch (error) {
