6d3dcc0a0a01f6f74d99c2f94b37451aadcbaaba
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-03-09 11:15:58 -0300

    corrigindo URL

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,8 +2,8 @@ const { createPool } = require('mysql2/promise');
 require('dotenv').config();
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'monorail.proxy.rlwy.net',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.HOSTNAME || 'https://kptransportes-backend.up.railway.app/',
+  port: process.env.MYSQL_PORT || '11699',
   database: process.env.MYSQL_DB_NAME || 'railway',
   username: process.env.MYSQL_USER || 'mysql',
   password: process.env.MYSQL_PASSWORD || 'password',
@@ -17,8 +17,8 @@ const options = {
 // dbConfig.js
 
 const pool = createPool({
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'monorail.proxy.rlwy.net',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.HOSTNAME || 'https://kptransportes-backend.up.railway.app/',
+  port: process.env.MYSQL_PORT || '11699',
   database: process.env.MYSQL_DB_NAME || 'railway',
   user: process.env.MYSQL_USER || 'mysql',
   password: process.env.MYSQL_PASSWORD || 'password',
