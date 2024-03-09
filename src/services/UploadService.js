6769030b34b213b56d31ea06862f632df738f538
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-03-09 18:01:11 -0300

    adicionando o comando no script

--- src/services/UploadService.js
+++ src/services/UploadService.js
@@ -6,11 +6,8 @@ const processUpload = async (files) => {
     // Caminho para o diretório "uploads"
     const uploadDirectory = path.join(__dirname, '..', '..', 'uploads');
 
-    console.log('pasta -=----->',uploadDirectory);
-
     // Crie o diretório "uploads" se não existir
     if (!fs.existsSync(uploadDirectory)) {
-      console.log('entrou no if?????');
       fs.mkdirSync(uploadDirectory);
     }
 
@@ -25,21 +22,6 @@ const processUpload = async (files) => {
     // Imprima uma mensagem ou retorne uma resposta ao frontend, se necessário
     console.log('Arquivos salvos em', uploadDirectory);
 
-    // Execute o comando "node xmlProcessor.js" aqui
-    const { exec } = require('child_process');
-    exec('node xmlProcessor.js', (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Erro ao executar xmlProcessor.js: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Erro ao executar xmlProcessor.js: ${stderr}`);
-        return;
-      }
-      console.log(`xmlProcessor.js executado com sucesso: ${stdout}`);
-    });
-
-    return { success: true, message: 'Arquivos processados com sucesso!' };
   } catch (error) {
     console.error('Erro ao processar os arquivos:', error);
     return { success: false, message: 'Erro ao processar os arquivos.' };
