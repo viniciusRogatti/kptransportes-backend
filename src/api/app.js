3e91b8bbded26d6316aeef8524ac3aa9a5273c65
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-03-10 17:38:34 -0300

    tentando outra abordagem para configurar o cors

--- src/api/app.js
+++ src/api/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://viniciusrogatti.github.io, http://localhost:3000',
+  origin: ['https://viniciusrogatti.github.io', 'http://localhost:3000'],
   credentials: false,
   exposedHeaders: ['Authorization'],
 }));
