2a79c0964e1a0bd445cc565acee82d9565489e4a
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-04-14 15:15:45 -0300

    depurando

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -20,7 +20,7 @@ const login = async (req, res) => {
     }
 
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: '8h',
     });
 
     res.json({ token });
--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -17,8 +17,7 @@ const getUserByUsername = async (username) => {
 }
 
 const verifyToken = async (token) => {
-  console.log('TOKEN===================>', token);
-  console.log('JWT_SECRET:', process.env.JWT_SECRET);
+  console.log('TOKEN===================>', typeof token);
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log('DECODE ----------->',decoded);
