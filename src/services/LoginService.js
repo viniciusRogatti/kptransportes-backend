950109e3888eb39239fed78343f10008e3ceac82
Marcos Vinicius <vinnyzsouza@gmail.com>
2024-04-14 15:36:33 -0300

    ultima tentativa

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -17,17 +17,16 @@ const getUserByUsername = async (username) => {
 }
 
 const verifyToken = async (token) => {
-  console.log('TOKEN===================>', typeof token);
   try {
-    const decoded = jwt.verify(Buffer.from(token, 'utf-8'), process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log('DECODE ----------->',decoded);
-    // const user = await User.findByPk(decoded.id);
+    const user = await User.findByPk(decoded.id);
 
-    // if (!user) {
-    //   throw new Error('Usuário não encontrado');
-    // }
+    if (!user) {
+      throw new Error('Usuário não encontrado');
+    }
 
-    // return user;
+    return user;
   } catch (error) {
     console.error('Erro ao verificar o token:', error);
     throw new Error('Token inválido');
